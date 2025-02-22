48c4121dbac88ad33e5ef00f11b9a5397f08639e
Shuvrajyoti <shuvra458@gmail.com>
2025-02-22 21:21:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
 
-            fetch('http://127.0.0.1:8000/api/login', {
+            fetch('inventory-management-production-a492.up.railway.app/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
